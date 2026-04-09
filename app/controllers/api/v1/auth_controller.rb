1e7a9f641764e2543490b73ed8af47f99337e04c
anpham <phamhongan2201@gmail.com>
2026-04-09 20:06:48 +0700

    x

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -9,6 +9,8 @@ module Api
           # Generate JWT token
           token = JsonWebToken.encode(user_id: user.id)
           time = Time.now + 24.hours.to_i
+
+          
           render json: { 
             token: token, 
             exp: time.strftime("%m-%d-%Y %H:%M"),
