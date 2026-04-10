659161f47689f709113d0f7ac7b09ab09a0eba0e
anpham <phamhongan2201@gmail.com>
2026-04-10 11:15:12 +0700

    x

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -10,6 +10,7 @@ module Api
           token = JsonWebToken.encode(user_id: user.id)
           time = Time.now + 24.hours.to_i
 
+          
           render json: { 
             token: token, 
             exp: time.strftime("%m-%d-%Y %H:%M"),
